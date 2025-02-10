d8e0fd45c5e93cc7d0a2b4ca21ce851572bd3c51
Soham <sohamdhiman3@gmail.com>
2025-02-10 21:45:20 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,3 +1,50 @@
+// #include<stdio.h>
+// int main(){
+//     int a=0;
+//     for (int i=1;i<=10;i++){
+//         a+=i;
+//     }
+// printf("%d",a);
+// }
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     scanf("%d",&a);
+//     for (int i=1;i<=10;i++){
+//         printf(" %d*%d=%d ",a,i,(a*i));
+//     }
+// }
+// #include<stdio.h>
+// int main(){
+//     int a,n;
+//     scanf("%d %d",&a,&n);
+//     for (int i=1;i<=n;i++){
+//         printf(" %d*%d=%d \n",a,i,(a*i));
+//     }
+// }
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     int b=1;
+//     scanf("%d",&a);
+//     for (int i=1;i<=a;i++){
+//         b=b*i;
+//     }
+//     printf("%d",b);
+// }
+// #include <stdio.h>
+
+// int main() {
+//     int a,b;
+//     scanf("%d %d",&a,&b);
+//     if((a*a)=b){
+//         printf("Yes");
+//     }
+//     else{
+//         printf("No");
+//     }
+//     return 0;
+// }
 #include <stdio.h>
 int main() {
     int a,b,d;
@@ -17,8 +64,13 @@ int main() {
     }
     else if(c=='/'){
         float e;
-        e=a / b;
-        printf("%.0f",e);
+        if (b==0){
+            printf("");
+        }
+        else{
+            e=a / b;
+            printf("%.0f",e);
+        }
     }
     else{
         printf("Invalid");
